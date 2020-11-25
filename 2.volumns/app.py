656bff2e7e1efbb265bf6a69afd6ec1cbda9f426
wj-Mcat <1435130236@qq.com>
2020-11-25 14:45:28 +0800

    init the base code

---
+++ 2.volumns/app.py
@@ -0,0 +1,7 @@
+import time
+from datetime import datetime
+
+# bala bala
+while True:
+    time.sleep(1)
+    print(datetime.now())
---
+++ 2.volumns/build.sh
@@ -0,0 +1,2 @@
+docker build -t mount-test:latest .
+docker run -it mount-test:latest ls /app/mount-test sdsdofij
